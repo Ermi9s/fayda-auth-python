c195bc43adcad22285af45d1d07aecc1ab0a5920
ermi9s <ermiayele1122334477@gmail.com>
2025-08-02 12:03:28 +0300

    small fix: redis

--- fayda_auth/config/redis.py
+++ fayda_auth/config/redis.py
@@ -7,41 +7,28 @@ class RedisConfig:
         self.redis_client = None
 
     def init_redis(self):
-        redis_host = os.getenv('REDIS_HOST', 'localhost')
-        redis_port = os.getenv('REDIS_PORT', '6379')
-        
-        # self.redis_client = redis.Redis(host=redis_host, port=int(redis_port), db=0)
-        self.redis_client = redis.Redis(
-                            host='redis-12565.c8.us-east-1-2.ec2.redns.redis-cloud.com',
-                            port=12565,
-                            decode_responses=True,
-                            username="default",
-                            password="k6hJ8j31Lm3O3iXMdhsjKzjefzxBxl8U",
-                        )
-    
+        # redis_host = os.getenv('REDIS_HOST', 'redis-12565.c8.us-east-1-2.ec2.redns.redis-cloud.com')
+        # redis_port = int(os.getenv('REDIS_PORT', '12565'))
+        # redis_username = os.getenv('REDIS_USERNAME', 'default')
+        # redis_password = os.getenv('REDIS_PASSWORD', 'k6hJ8j31Lm3O3iXMdhsjKzjefzxBxl8U')
+
+        redis_host =  'redis-12565.c8.us-east-1-2.ec2.redns.redis-cloud.com'
+        redis_port = 12565
+        redis_username = 'default'
+        redis_password = 'k6hJ8j31Lm3O3iXMdhsjKzjefzxBxl8U'
+
         try:
+            self.redis_client = redis.Redis(
+                host=redis_host,
+                port=redis_port,
+                decode_responses=True,
+                username=redis_username,
+                password=redis_password
+            )
             self.redis_client.ping()
             logging.info("Connected to Redis successfully!")
         except redis.ConnectionError as e:
             logging.error(f"Failed to connect to Redis: {e}")
             raise
+
         return self.redis_client
-    
-
-
-
-# r = redis.Redis(
-#     host='redis-12565.c8.us-east-1-2.ec2.redns.redis-cloud.com',
-#     port=12565,
-#     decode_responses=True,
-#     username="default",
-#     password="k6hJ8j31Lm3O3iXMdhsjKzjefzxBxl8U",
-# )
-
-# success = r.set('foo', 'bar')
-# # True
-
-# result = r.get('foo')
-# print(result)
-# # >>> bar
-
